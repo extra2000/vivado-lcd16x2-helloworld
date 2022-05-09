c5e1c9a356cc05f42952b11ab58edb4e6a5b4534
Nik Mohamad Aizuddin <nick@extra2000.io>
2022-05-09 17:55:08 +0800

    perf(timing): improve timing

--- vivado/src/verilog/lcd16x2.v
+++ vivado/src/verilog/lcd16x2.v
@@ -10,13 +10,13 @@
 module lcd16x2 #(
   parameter [31:0] CLK_FREQ       = 125000000,   // 125MHz
   // Timing cycles
-  parameter [19:0]  T_40_NS       = $ceil(0.000000040 * CLK_FREQ),
-  parameter [19:0]  T_250_NS      = $ceil(0.000000250 * CLK_FREQ),
-  parameter [19:0]  T_42_US       = $ceil(0.000042000 * CLK_FREQ),
-  parameter [19:0]  T_60_US       = $ceil(0.000060000 * CLK_FREQ),
-  parameter [19:0]  T_100_US      = $ceil(0.000100000 * CLK_FREQ),
-  parameter [19:0]  T_5000_US     = $ceil(0.005000000 * CLK_FREQ),
-  parameter [19:0]  T_100_MS      = $ceil(0.100000000 * CLK_FREQ),
+  parameter [31:0]  T_40_NS       = $ceil(0.000000040 * CLK_FREQ),
+  parameter [31:0]  T_250_NS      = $ceil(0.000000250 * CLK_FREQ),
+  parameter [31:0]  T_42_US       = $ceil(0.000042000 * CLK_FREQ),
+  parameter [31:0]  T_60_US       = $ceil(0.000060000 * CLK_FREQ),
+  parameter [31:0]  T_100_US      = $ceil(0.000100000 * CLK_FREQ),
+  parameter [31:0]  T_5000_US     = $ceil(0.005000000 * CLK_FREQ),
+  parameter [31:0]  T_60_MS       = $ceil(0.060000000 * CLK_FREQ),
   // Commands
   parameter [7:0]   CMD_SETUP     = 8'b00111000,  // Set 8-bit, 2-line, 5x7 dots
   parameter [7:0]   CMD_DISP_ON   = 8'b00001100,  // Turn ON display
@@ -39,14 +39,14 @@ module lcd16x2 #(
   ////////////////////////////////////////////////////////////////////////////////////////////////
   // Counting mechanism
   ////////////////////////////////////////////////////////////////////////////////////////////////
-  reg [19:0] cnt_timer     = 0;
+  reg [31:0] cnt_timer     = 0;
   reg        flag_40_ns    = 0;
   reg        flag_250_ns   = 0;
   reg        flag_42_us    = 0;
   reg        flag_60_us    = 0;
   reg        flag_100_us   = 0;
   reg        flag_5000_us  = 0;
-  reg        flag_100_ms   = 0;
+  reg        flag_60_ms    = 0;
   reg        flag_rst      = 1;  // start with RST flag set, so the counting can't be started
 
   always @(posedge clk_i) begin
@@ -57,7 +57,7 @@ module lcd16x2 #(
       flag_60_us    <= 0;
       flag_100_us   <= 0;
       flag_5000_us  <= 0;
-      flag_100_ms   <= 0;
+      flag_60_ms    <= 0;
       cnt_timer     <= 0;
     end else begin
       if (cnt_timer >= T_40_NS) begin
@@ -78,8 +78,8 @@ module lcd16x2 #(
       if (cnt_timer >= T_5000_US) begin
         flag_5000_us <= 1;
       end
-      if (cnt_timer >= T_100_MS) begin
-        flag_100_ms <= 1;
+      if (cnt_timer >= T_60_MS) begin
+        flag_60_ms <= 1;
       end
       cnt_timer <= cnt_timer + 1;
     end
@@ -99,7 +99,7 @@ module lcd16x2 #(
       0: begin
         rdy_o <= 0;
         if (substate == 0) begin
-          if (!flag_100_ms) begin
+          if (!flag_60_ms) begin
             flag_rst <= 0;
           end else begin
             flag_rst <= 1;
